f9bbe0455e8660b7f8b0db294b382e6b44a1d1e0
hyzoon <143889446+shj1081@users.noreply.github.com>
2024-01-26 01:32:15 +0900

    [Silver IV] Title: 듣보잡, Time: 40 ms, Memory: 7580 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.cc
+++ 백준/Silver/1764. 듣보잡/듣보잡.cc
@@ -1,32 +1,43 @@
-#include <iostream>
 #include <algorithm>
-#include <vector>
+#include <iostream>
 #include <set>
+#include <vector>
+
 using namespace std;
 
 int main() {
-	int N, M;
-	int count = 0;
-	string see, listen;
-	set <string> set;
-	vector <string> result;
-	cin >> N >> M;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-	for (int i = 0; i < N; i++) {
-		cin >> see;
-		set.insert(see);
-	}
-	for (int i = 0; i < M; i++) {
-		cin >> listen;
-		if (set.find(listen) != set.end()) {
-			count++;
-			result.push_back(listen);
-		}
-	}
-	sort(result.begin(), result.end());
+    // set을 이용한 풀이
+    set<string> set;
+    vector<string> arr;
 
-	cout << count << '\n';
-	for (int i = 0; i < result.size(); i++) {
-		cout << result[i] << '\n';
-	}
+    int N, M;
+    int cnt = 0;
+    string name;
+
+    cin >> N >> M;
+
+    for (int i = 0; i < N; i++) {
+        cin >> name;
+        set.insert(name);
+    }
+
+    for (int i = 0; i < M; i++) {
+        cin >> name;
+        if (set.find(name) != set.end()) {
+            arr.push_back(name);
+            cnt++;
+        }
+    }
+
+    sort(arr.begin(), arr.end());
+
+    cout << cnt << "\n";
+    for (size_t i = 0; i < arr.size(); i++) {
+        cout << arr[i] << "\n";
+    }
+    return 0;
 }
