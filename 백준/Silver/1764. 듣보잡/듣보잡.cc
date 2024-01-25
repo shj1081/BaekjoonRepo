d2673c914807c46b13a3ade3d3473f6ac14a1024
hyzoon <143889446+shj1081@users.noreply.github.com>
2024-01-26 01:29:13 +0900

    [Silver IV] Title: 듣보잡, Time: 388 ms, Memory: 7448 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.cc
+++ 백준/Silver/1764. 듣보잡/듣보잡.cc
@@ -1,39 +1,43 @@
 #include <algorithm>
 #include <iostream>
-#include <map>
+#include <set>
 #include <string>
 #include <vector>
 
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
+    // ios::sync_with_stdio(0);
+    // cin.tie(0);
+    // cout.tie(0);
 
-    map<string, int> maps;
+    // set을 이용한 풀이
+    set<string> set;
     vector<string> arr;
 
     int N, M;
+    int cnt = 0;
     string name;
+
     cin >> N >> M;
 
     for (size_t i = 0; i < N; i++) {
         cin >> name;
-        maps[name] = 1;
+        set.insert(name);
     }
 
     for (size_t i = 0; i < M; i++) {
         cin >> name;
-        if (maps[name] == 1) {
+        if (set.find(name) != set.end()) {
             arr.push_back(name);
+            cnt++;
         }
     }
 
     sort(arr.begin(), arr.end());
 
-    cout << arr.size() << endl;
-    for (int i = 0; i < arr.size(); i++) {
+    cout << cnt << endl;
+    for (size_t i = 0; i < arr.size(); i++) {
         cout << arr[i] << endl;
     }
     return 0;
