99746fe4cf68ffd43388499570b612436afd0627
hyzoon <143889446+shj1081@users.noreply.github.com>
2024-01-30 14:50:31 +0900

    [Bronze IV] Title: 녹색거탑, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.cc
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int N;
+    cin >> N;
+
+    cout << pow(2, N);
+
+    return 0;
+}
