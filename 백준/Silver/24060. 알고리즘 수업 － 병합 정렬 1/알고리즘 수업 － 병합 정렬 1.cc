639a6934228a84cb6f393c45d7b692136a452075
hyzoon <143889446+shj1081@users.noreply.github.com>
2024-02-01 03:40:58 +0900

    [Silver III] Title: 알고리즘 수업 - 병합 정렬 1, Time: 144 ms, Memory: 6152 KB -BaekjoonHub

---
+++ 백준/Silver/24060. 알고리즘 수업 － 병합 정렬 1/알고리즘 수업 － 병합 정렬 1.cc
@@ -0,0 +1,73 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int cnt = 0;
+int K;
+
+void merge_arr(vector<int> &vec, vector<int> &tmp, int p, int q, int r) {
+    int i = p;
+    int j = q + 1;
+    int t = 0;
+    tmp.clear();
+
+    while (i <= q && j <= r) {
+        if (vec[i] <= vec[j]) {
+            tmp.push_back(vec[i++]);
+        } else {
+            tmp.push_back(vec[j++]);
+        }
+    }
+
+    while (i <= q) {
+        tmp.push_back(vec[i++]);
+    }
+    while (j <= r) {
+        tmp.push_back(vec[j++]);
+    }
+
+    i = p;
+    while (i <= r) {
+        cnt++;
+        if (cnt == K) {
+            cout << tmp[t];
+            return;
+        }
+        vec[i++] = tmp[t++];
+    }
+}
+
+void merge_sort(vector<int> &vec, vector<int> &tmp, int p, int r) {
+    if (p < r) {
+        int q = (p + r) / 2;
+        merge_sort(vec, tmp, p, q);
+        merge_sort(vec, tmp, q + 1, r);
+        merge_arr(vec, tmp, p, q, r);
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int N, num;
+    vector<int> vec;
+    vector<int> tmp;
+
+    cin >> N >> K;
+    for (size_t i = 0; i < N; i++) {
+        cin >> num;
+        vec.push_back(num);
+    }
+
+    tmp.resize(N);
+    merge_sort(vec, tmp, 0, vec.size() - 1);
+    if (cnt < K) {
+        cout << -1;
+    }
+
+    return 0;
+}
