17d6294bab299b794cbbc26e43f9089d5e2942df
hyzoon <143889446+shj1081@users.noreply.github.com>
2024-02-01 05:40:34 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 144 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.cc
@@ -0,0 +1,28 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void hanoi(int N, int from, int to, int via) {
+    if (N == 1) {
+        cout << from << " " << to << "\n";
+        return;
+    }
+    hanoi(N - 1, from, via, to);        // N-1개를 경유지로 옮김
+    cout << from << " " << to << "\n";  // N번째를 목적지로 옮김
+    hanoi(N - 1, via, to, from);        // 경유지에 있는 N-1개를 목적지로 옮김
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N;
+    cin >> N;
+    cout << (1 << N) - 1 << "\n";  // 2^N - 1
+    hanoi(N, 1, 3, 2);
+
+    return 0;
+}
