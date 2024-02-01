fa5a104182048c30eaae34b60155166b44cf5592
hyzoon <143889446+shj1081@users.noreply.github.com>
2024-02-01 15:41:10 +0900

    [Silver III] Title: N과 M (3), Time: 348 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> res;
+
+void backTrack(int N, int M, int index) {
+    // 모두 선택했다면 출력
+    if (index == M) {
+        for (size_t i = 0; i < M; i++) {
+            cout << res[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+
+    for (size_t i = 0; i < N; i++) {
+        // 만약 num이 선택되지 않은 상태라면 선택하고 res에 추가
+        res[index] = i + 1;
+        backTrack(N, M, index + 1);  // num i가 선택된 경우를 재귀적으로 생각
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int N, M;
+    cin >> N >> M;
+
+    res.resize(M);
+
+    backTrack(N, M, 0);
+
+    return 0;
+}
