56ed0be8046e32df8cd0a17fd98c04408274bdc5
hyzoon <143889446+shj1081@users.noreply.github.com>
2024-02-17 17:03:18 +0900

    Update 히스토그램에서 가장 큰 직사각형.cc

--- 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.cc
+++ 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.cc
@@ -11,14 +11,22 @@ long long int maxRectangle(int left, int right) {
         return histogram[left];
     }
 
-    // divide
-    int mid = (left + right) / 2; 
+    /*
+     * divide
+        - width가 1이 될 때까지 나누어준다.
+        - 분할된 각각의 부분에 대해 최대 넓이를 구한다.
+        - 분할선을 포함하는 경우와 포함하지 않는 경우를 나누어서 비교한다.
+     */
+    int mid = (left + right) / 2;
     long long int result = max(maxRectangle(left, mid), maxRectangle(mid + 1, right));
     int lo = mid, hi = mid + 1;
     long long int height = min(histogram[lo], histogram[hi]);
     result = max(result, height * 2);
 
-    // conquer
+    /*
+     * conquer
+     * - 위에서 구한 최대 넓이를 기준으로 왼쪽과 오른쪽을 확장해나간다.
+     */
     while (left < lo || hi < right) {
         if (hi < right && (lo == left || histogram[lo - 1] < histogram[hi + 1])) {
             hi++;
@@ -38,17 +46,12 @@ int main() {
     cout.tie(0);
 
     int numOfRectangles;
-    while (true) {
-        cin >> numOfRectangles;
-        if (numOfRectangles == 0) {
-            break;
-        }
-        histogram.resize(numOfRectangles);
-        for (int i = 0; i < numOfRectangles; i++) {
-            cin >> histogram[i];
-        }
-        cout << maxRectangle(0, numOfRectangles - 1) << endl;
+    cin >> numOfRectangles;
+    histogram.resize(numOfRectangles);
+    for (int i = 0; i < numOfRectangles; i++) {
+        cin >> histogram[i];
     }
+    cout << maxRectangle(0, numOfRectangles - 1) << endl;
 
     return 0;
 }
