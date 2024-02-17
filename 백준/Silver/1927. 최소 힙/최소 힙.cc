ed5d36b9849f5695ec2e328e82d93d4fc07edd93
hyzoon <143889446+shj1081@users.noreply.github.com>
2024-02-17 17:10:32 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    priority_queue<int, vector<int>, greater<int>> pq;  // min heap
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        int cmd;
+        cin >> cmd;
+        if (cmd == 0) {
+            if (pq.empty()) {
+                cout << 0 << endl;
+            } else {
+                cout << pq.top() << endl;
+                pq.pop();
+            }
+        } else {
+            pq.push(cmd);
+        }
+    }
+
+    return 0;
+}
